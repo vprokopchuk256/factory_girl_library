351553095a757bb8beedc2020e6480b55c4e063a
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-02-08 20:22:16 +0200

    library updated

--- lib/factory_girl_library/library.rb
+++ lib/factory_girl_library/library.rb
@@ -4,43 +4,52 @@ module FactoryGirlLibrary
 
     def initialize
       @library = {}.with_indifferent_access
-      @connection = ActiveRecord::Base.connection.clone
+      @registration_queue = []
     end
-   
-    def create name, opts 
-      unless @library[name]
-        Thread.new{ @library[name] = ::FactoryGirl.create(name) }.join
-      end
 
-      update(@library[name], opts)
+    def register name, object
+      @library[name] = object
+      @registration_queue.push(object)
+    end
+
+    def registered? name
+      @library.has_key?(name)
+    end
+
+    def get name, opts = {}
+      @library[name].tap do |object|
+        update(object, opts)
+      end
+    end
+
+    def reload name
+      @library[name].try(:reload)
     end
 
     def clear
       @library.clear
-    end
 
-    def reload(obj)
-      if name = @library.key(obj) and obj.respond_to?(:reload)
-        @library[name] = obj.reload
+      while object = @registration_queue.pop
+        object.destroy
       end
     end
 
     private
 
-
     def update obj, opts
-      if opts.present?
+      if obj && opts.present?
         opts.each do |key, value|
           obj.send("#{key}=", value)
         end
+
+        obj.save!
       end
 
-      obj.save!
       obj
     end
     
     class << self
-      delegate :create, :clear, :reload, to: :instance
+      delegate :register, :registered?, :reload, :get, :clear, to: :instance
     end
   end
 end
--- spec/factory_girl_library/library_spec.rb
+++ spec/factory_girl_library/library_spec.rb
@@ -3,55 +3,114 @@ require 'spec_helper'
 require 'factory_girl_library/library'
 
 describe FactoryGirlLibrary::Library do
-  describe '#create' do
-    def create opts = {}
-      described_class.create(:post, opts)
+  before { described_class.clear }
+
+  describe '#get' do
+    subject { described_class.get(:post, title: :new_title) }
+
+    describe "when object is registered" do
+      before { described_class.register(:post, FactoryGirl.create(:post)) }
+      
+      it { is_expected.not_to be_blank }
+      its(:title) { is_expected.to eq('new_title') }
     end
 
-    subject { create(title: 'title') }
-
-    before do
-      expect(::FactoryGirl).to receive(:create).with(:post).once.and_call_original
-    end
-
-    it { is_expected.to be_a_kind_of(Post) }
-    its(:title) { is_expected.to eq('title') }
-
-    it 'does not create send instance' do
-      2.times { create }
-    end
-
-    describe 'with transaction' do
-      describe 'inside transaction' do
-        it 'creates visible instance' do
-          ActiveRecord::Base.connection.transaction(isolation: :read_committed) do
-            expect { create }.to change(Post, :count).from(0).to(1)
-          end
-        end
-      end
-
-      describe 'when transaction is rolled back' do
-        it 'remains in database' do
-          expect {
-            ActiveRecord::Base.connection.transaction do
-              create
-              raise ActiveRecord::Rollback
-            end
-          }.to change(Post, :count).by(1)
-        end
-
-        it 'reverts updated property back' do
-          ActiveRecord::Base.connection.transaction do
-            expect(create(title: 'updated_title').title). to eq('updated_title')
-            expect(Post.first.title).to eq('updated_title')
-
-            raise ActiveRecord::Rollback
-          end
-          
-          expect(create.title). to eq('Some Interesting title')
-          expect(Post.first.title).to eq('Some Interesting title')
-        end
-      end
+    describe "when object is not registered" do
+      it { is_expected.to be_blank }
     end
   end
+
+  describe '#reload' do
+    let(:post) { FactoryGirl.create(:post) }
+
+    before { described_class.register(:post, post) }
+
+    it 'reloads registered object' do
+      expect(post).to receive(:reload)
+      described_class.reload(:post)
+    end
+  end
+
+  describe '#registered?' do
+    subject { described_class.registered?(:post) }
+
+    describe "when object is registered" do
+      before { described_class.register(:post, FactoryGirl.create(:post)) }
+      
+      it { is_expected.to be_truthy }
+    end
+
+    describe "when object is not registered" do
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe '#clear' do
+    let(:post) { FactoryGirl.create(:post) }
+    let(:post_1) { FactoryGirl.create(:post) }
+
+    before do
+      described_class.register(:post, post)
+      described_class.register(:post_1, post_1)
+    end
+
+    it 'destroys registered object in reverse to registration order' do
+      expect(post_1).to receive(:destroy).ordered
+      expect(post).to receive(:destroy).ordered
+
+      described_class.clear
+    end
+  end
+
+  # describe '#create' do
+  #   def create opts = {}
+  #     described_class.create(:post, opts)
+  #   end
+
+  #   subject { create(title: 'title') }
+
+  #   before do
+  #     expect(::FactoryGirl).to receive(:create).with(:post).once.and_call_original
+  #   end
+
+  #   it { is_expected.to be_a_kind_of(Post) }
+  #   its(:title) { is_expected.to eq('title') }
+
+  #   it 'does not create send instance' do
+  #     2.times { create }
+  #   end
+
+  #   describe 'with transaction' do
+  #     describe 'inside transaction' do
+  #       it 'creates visible instance' do
+  #         ActiveRecord::Base.connection.transaction(isolation: :read_committed) do
+  #           expect { create }.to change(Post, :count).from(0).to(1)
+  #         end
+  #       end
+  #     end
+
+  #     describe 'when transaction is rolled back' do
+  #       it 'remains in database' do
+  #         expect {
+  #           ActiveRecord::Base.connection.transaction do
+  #             create
+  #             raise ActiveRecord::Rollback
+  #           end
+  #         }.to change(Post, :count).by(1)
+  #       end
+
+  #       it 'reverts updated property back' do
+  #         ActiveRecord::Base.connection.transaction do
+  #           expect(create(title: 'updated_title').title). to eq('updated_title')
+  #           expect(Post.first.title).to eq('updated_title')
+
+  #           raise ActiveRecord::Rollback
+  #         end
+          
+  #         expect(create.title). to eq('Some Interesting title')
+  #         expect(Post.first.title).to eq('Some Interesting title')
+  #       end
+  #     end
+  #   end
+  # end
 end
