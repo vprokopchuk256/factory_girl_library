00390035b46b86c7662f1a6682254445cd0b6287
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-02-23 18:53:46 -0500

    library options are always optional

--- lib/factory_girl_library/factory_girl/syntax/methods_decorator.rb
+++ lib/factory_girl_library/factory_girl/syntax/methods_decorator.rb
@@ -2,8 +2,9 @@ module FactoryGirlLibrary
   module FactoryGirl
     module Syntax
       module MethodsDecorator
-        def library name, opts
+        def library name, opts = {}
           ::FactoryGirl.library(name, opts)
+        }
         end
       end
     end
